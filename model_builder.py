280702f28c4d4837fdcc010b1f03dbe523efad3f
Mahad <mahadkhaliqjbd@gmail.com>
2025-11-07 15:51:18 -0600

    updated model_builder with regularizer

--- model_builder.py
+++ model_builder.py
@@ -2,17 +2,8 @@ from tensorflow import keras
 import numpy as np
 
 def build_model(architecture, search_space, input_shape=(28, 28, 1), 
-                num_classes=10, learning_rate=0.001):
-    """
-    Build a model from architecture specification
-    
-    Args:
-        architecture: List of layer specifications
-        search_space: Dictionary of operations
-        input_shape: Input image shape
-        num_classes: Number of output classes
-        learning_rate: Learning rate for optimizer
-    """
+                num_classes=10, learning_rate=0.001, dropout_rate=0.5):
+    """Build model with stronger regularization"""
     inputs = keras.Input(shape=input_shape)
     x = inputs
     
@@ -31,19 +22,21 @@ def build_model(architecture, search_space, input_shape=(28, 28, 1),
         for layer in layers_list:
             x = layer(x)
     
-    # Use GlobalAveragePooling instead of Flatten for efficiency
+    # Global pooling
     if x.shape[1] > 1:
         x = keras.layers.GlobalAveragePooling2D()(x)
     else:
         x = keras.layers.Flatten()(x)
     
-    x = keras.layers.Dense(128, activation='relu')(x)
-    x = keras.layers.Dropout(0.3)(x)
+    # Add stronger regularization
+    x = keras.layers.Dropout(dropout_rate)(x)  # Increased from 0.3
+    x = keras.layers.Dense(128, activation='relu', 
+                          kernel_regularizer=keras.regularizers.l2(0.001))(x)
+    x = keras.layers.Dropout(dropout_rate)(x)
     outputs = keras.layers.Dense(num_classes, activation='softmax')(x)
     
     model = keras.Model(inputs=inputs, outputs=outputs)
     
-    # Use Adam with configurable learning rate
     optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
     
     model.compile(
